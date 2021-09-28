c651be40b8bc8f2675f8a65655adfdc6319b8460
AnastasiaSvyat <64147723+AnastasiaSvyat@users.noreply.github.com>
2021-09-28 23:59:35 +0300

    add changes in CalendarComponentAdmin

--- model/Employee.js
+++ model/Employee.js
@@ -1,51 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// const mongoosePaginate = require("mongoose-paginate-v2");
 const mongoosePaginate = require('mongoose-paginate-v2');
 
 
-// let Employee = new Schema({
-//   name: {
-//     type: String
-//   },
-//   position:{
-//     type: String
-//   },
-//   birthday:{
-//     type: String
-//   },
-//   salary:{
-//     type: Number
-//   },
-//   phone:{
-//     type: Number
-//   },
-//   email: {
-//     type: String
-//   },
-//   password: {
-//     type: String
-//   },
-//   token: {
-//     type: String
-//   },
-//   role: {
-//     type: String
-//   },
-//   info: {
-//     type:Array
-//   },
-//   toDoList: {
-//     type:Array
-//   },
-//   file: {
-//     type:Array
-//   }
-// }, {
-//   collection: 'employee'
-// })
+
 const Employee = new mongoose.Schema({
-  /* your schema definition */
   name: {
     type: String
   },
